b6d8ad0982347fa275e2dd50f94d30fc117fa1a4
Skillvendor <skillvendor193@gmail.com>
2022-03-25 15:45:06 +0200

    respond with json

--- animal/hello.go
+++ animal/hello.go
@@ -1,9 +1,23 @@
 package animal
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
+type Human struct {
+	Name string
+	Age  int
+}
+
 func MyFunc(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("YES"))
+	myAnimal := Human{Name: "Gigel", Age: 25}
+
+	err := json.NewEncoder(w).Encode(myAnimal)
+
+	if err != nil {
+		fmt.Println("ERROR2", err)
+	}
+	// w.Write([]byte(myAnimal.Owner[0].Name))
 }
