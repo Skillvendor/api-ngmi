25bb4eff6380c7c7e1459b542e28c27dd5a448ca
Skillvendor <Skillvendor193@gmail.com>
2022-07-02 06:05:03 +0000

    no need to verify it's a post

--- api/user/main.go
+++ api/user/main.go
@@ -12,44 +12,38 @@ import (
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		newUser := models.User{}
+	newUser := models.User{}
 
-		err := json.NewDecoder(r.Body).Decode(&newUser)
+	err := json.NewDecoder(r.Body).Decode(&newUser)
 
-		if err != nil {
-			json.NewEncoder(w).Encode(types.StandardError{Message: "Error Decoding User"})
-			w.WriteHeader(http.StatusInternalServerError)
-		}
+	if err != nil {
+		json.NewEncoder(w).Encode(types.StandardError{Message: "Error Decoding User"})
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 
-		if newUser.Address == "" {
-			json.NewEncoder(w).Encode(types.StandardError{Message: "No Address Given"})
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		newUser.Find()
-
-		if newUser.Id == 0 {
-			// user doesn't exist so we try creating
-			rand.Seed(time.Now().UnixNano())
-			newUser.Nonce = strconv.Itoa(rand.Int())
-			newUser.AccessLevel = 1
-			newUser.AuthToken = ""
-			newUser.Save()
-		}
-
-		err = json.NewEncoder(w).Encode(newUser)
-
-		if err != nil {
-			json.NewEncoder(w).Encode(types.StandardError{Message: "Error Encoding User"})
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	default:
-		json.NewEncoder(w).Encode(types.StandardError{Message: "Post method"})
+	if newUser.Address == "" {
+		json.NewEncoder(w).Encode(types.StandardError{Message: "No Address Given"})
 		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	newUser.Find()
+
+	if newUser.Id == 0 {
+		// user doesn't exist so we try creating
+		rand.Seed(time.Now().UnixNano())
+		newUser.Nonce = strconv.Itoa(rand.Int())
+		newUser.AccessLevel = 1
+		newUser.AuthToken = ""
+		newUser.Save()
+	}
+
+	err = json.NewEncoder(w).Encode(newUser)
+
+	if err != nil {
+		json.NewEncoder(w).Encode(types.StandardError{Message: "Error Encoding User"})
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 }
 
