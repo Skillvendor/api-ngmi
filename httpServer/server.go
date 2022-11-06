23cffdf05a6e1a4bb563a36e3917a8160f7a566e
Skillvendor <Skillvendor193@gmail.com>
2022-11-06 16:51:40 +0200

    Update server.go

--- httpServer/server.go
+++ httpServer/server.go
@@ -30,6 +30,7 @@ func Run() {
 		"https://skillvendor-magiccarpet-fgfcbk4udpe.ws-eu74.gitpod.io",
 		"https://3000-skillvendor-magiccarpet-fgfcbk4udpe.ws-eu74.gitpod.io",
 		"https://3000-skillvendor-magiccarpet-63atwa9jrlv.ws-eu74.gitpod.io",
+		"https://3000-skillvendor-magiccarpet-hnmd8msbdxw.ws-eu74.gitpod.io",
 	}
 
 	if env == "prod" {
