0adfefa3f51b6f64f5b2358dd367f68dc0b4585f
Skillvendor <Skillvendor193@gmail.com>
2023-01-21 06:45:42 +0000

    Added community payments

---
+++ models/communitypayment.go
@@ -0,0 +1,86 @@
+package models
+
+import (
+	"api-ngmi/dbConn"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+)
+
+type CommunityPayment struct {
+	Id              int    `pg:"id" json:"-"`
+	CitizenWallet   string `pg:"citizen_wallet" json:"citizen_wallet" validate:"required"`
+	AccessWallet    string `pg:"access_wallet" json:"access_wallet" validate:"required"`
+	TransactionLink string `pg:"transaction_link" json:"transaction_link" validate:"required"`
+
+	DiscordHandle string `pg:"discord_handle" json:"discord_handle" validate:"required"`
+	TwitterHandle string `pg:"twitter_handle" json:"twitter_handle" validate:"required"`
+
+	TransactionVerified bool `pg:"transaction_verified" json:"transaction_verified"`
+	TransactionValid    bool `pg:"transaction_valid" json:"transaction_valid"`
+	Valid               bool `pg:"valid" json:"valid"`
+
+	ExpiresAt     time.Time `pg:"expires_at" json:"expires_at"`
+	CommunityName string    `pg:"community_name" json:"community_name"`
+}
+
+func (payment *CommunityPayment) Save() bool {
+	_, err := dbConn.DB.Model(payment).Insert()
+	if err != nil {
+		log.Println("Can't insert CommunityPayment", err)
+		return false
+	}
+
+	return true
+}
+
+func (payment *CommunityPayment) Update() bool {
+	_, err := dbConn.DB.Model(payment).Column("transaction_verified", "transaction_valid", "valid").WherePK().Update()
+	if err != nil {
+		log.Println("Can't insert CommunityPayment", err)
+		return false
+	}
+
+	return true
+}
+
+func (payment *CommunityPayment) Find() bool {
+	err := dbConn.DB.Model(payment).Where("lower(access_wallet) = ?", strings.ToLower(payment.AccessWallet)).First()
+
+	fmt.Println("Are there errors?", err)
+	return err == nil
+}
+
+func GetAllPaymentsCommunity() []CommunityPayment {
+	got := []CommunityPayment{}
+
+	err := dbConn.DB.Model(&got).Select()
+	if err != nil {
+		log.Println("Can't retrieve Payments", err)
+	}
+
+	return got
+}
+
+func GetAllPendingValidationPaymentsCommunity() []CommunityPayment {
+	got := []CommunityPayment{}
+
+	query := dbConn.DB.Model(&got).Where("transaction_verified = ?", false)
+
+	err := query.Select()
+	if err != nil {
+		log.Println("Can't retrieve Payments", err)
+	}
+
+	return got
+}
+
+// func (payment *NtPayment) Update() bool {
+// 	_, err := dbConn.DB.Model(payment).
+// 		Column("nonce", "auth_token").
+// 		WherePK().
+// 		Update()
+
+// 	return err == nil
+// }
--- services/user/main.go
+++ services/user/main.go
@@ -6,6 +6,7 @@ import (
 	"api-ngmi/models"
 	"api-ngmi/redis"
 	"fmt"
+	"time"
 )
 
 func TierFor(address string) int {
@@ -13,9 +14,11 @@ func TierFor(address string) int {
 
 	found := ntPayment.Find()
 
+	fmt.Println("This is what I found", found)
 	if found {
 		isS1, _ := cryptoEth.HasNTS1(ntPayment.CitizenWallet)
 
+		fmt.Println("Check if S1", isS1)
 		if isS1 {
 			fmt.Println("Has s1")
 			return constants.Gold
@@ -25,6 +28,20 @@ func TierFor(address string) int {
 		if isS2 {
 			// return utils.Max(constants.Silver, tier)
 			fmt.Println("Has s2")
+			return constants.Gold
+		}
+	}
+
+	communityPayment := models.CommunityPayment{AccessWallet: address}
+
+	foundCommunity := communityPayment.Find()
+
+	if foundCommunity {
+		today := time.Now()
+
+		expired := today.After(communityPayment.ExpiresAt)
+
+		if !expired {
 			return constants.Gold
 		}
 	}
